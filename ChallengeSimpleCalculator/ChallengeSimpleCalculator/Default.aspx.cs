051152ad2f49a3f16ce38f4f3fb144afe19ff397
Violette Allahham <vallahham@yahoo.com>
2016-09-14 17:13:17 -0500

    Comments added

--- ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
@@ -20,7 +20,7 @@ namespace ChallengeSimpleCalculator
             double secondNum = double.Parse(secondNumberTextBox.Text);
  
             double result = firstNum* secondNum;
-            resultLabel.Text = result.ToString();
+            resultLabel.Text = result.ToString();       // .Text is added onto whatever is needed to access the text. Also used as printing (cout)
         }
 
         protected void division_Button_Click(object sender, EventArgs e)
