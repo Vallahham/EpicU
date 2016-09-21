4a67730ec247e989d34cfedaf341d63394ed8185
Violette Allahham <vallahham@yahoo.com>
2016-09-21 17:19:34 -0500

    Update

--- CS-ASP_022/CS-ASP_022/Default.aspx.cs
+++ CS-ASP_022/CS-ASP_022/Default.aspx.cs
@@ -13,12 +13,37 @@ namespace CS_ASP_022
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            double[,] priceGrid = new double[3, 3];
+            priceGrid[0, 1] = 350.00;
+            priceGrid[0, 2] = 750.00;
+            priceGrid[1, 0] = 700.00;
+            priceGrid[1, 2] = 400.00;
+            priceGrid[2, 0] = 800.00;
+            priceGrid[2, 1] = 805.56;
 
+            //resultLabel.Text = priceGrid[1, 2].ToString();
         }
 
         protected void okButton_Click(object sender, EventArgs e)
         {
+            int fromCity;
+            if (fromChicagoRadio.Checked) fromCity = 0;
+            else if (fromNewYorkRadio.Checked) fromCity = 1;
+            else fromCity = 2;
 
+            int toCity;
+            if (toChicagoRadio.Checked) toCity = 0;
+            else if (toNewYorkRadio.Checked) toCity = 1;
+            else toCity = 2;
+
+          /*  if(fromCity == toCity)
+            {
+
+            }
+
+            resultLabel.Text = priceGrid[
+            */
+            
         }
     }
 }
