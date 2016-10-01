f38e3538a5b2c95f9c305eab9d79627e719fb559
Violette Allahham <vallahham@yahoo.com>
2016-10-01 10:47:53 -0500

    Update

---
+++ CS-ASP_036/CS-ASP_036/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS_ASP_036
+{
+    public class Car
+    {
+        public Car(string Maker)
+        {
+            Maker = make;
+        }
+
+        public string make { get; set; }
+        public string model { get; set; }
+        public  int year { get; set; }
+        public Wheel wheel { get; set; }
+
+        public double mileage(double currentMiles)
+        {
+            return currentMiles;
+        }
+
+
+    }
+
+    public class Wheel
+    {
+        public double size { get; set; }
+        public string brand { get; set; }
+
+    }
+
+}
