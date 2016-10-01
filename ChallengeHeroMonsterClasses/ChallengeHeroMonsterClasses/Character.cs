abb864d393db7d9c75a0f22e9c220f2170a7a4c0
Violette Allahham <vallahham@yahoo.com>
2016-10-01 13:33:05 -0500

    Update

---
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeHeroMonsterClasses
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public double Health { get; set; }
+        public double DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        
+
+
+    }
+}
