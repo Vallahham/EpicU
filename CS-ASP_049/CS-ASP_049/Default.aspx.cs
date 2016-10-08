031398642476d37366f240df4cc86d88ce92f10c
Violette Allahham <vallahham@yahoo.com>
2016-10-08 13:08:19 -0500

    Up

---
+++ CS-ASP_049/CS-ASP_049/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_049
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string result = "";
+
+            var cars = new Dictionary<string, Car> ()
+            {
+                { "V1", new Car { Make = "BMW", Model = "A", Color = "Black", Year = 2005}}
+            };
+
+
+            foreach (var pair in cars)
+            {
+                result += pair.Key + pair.Value.FormatDetailsForDisplay();
+            }
+
+
+            resultLabel.Text = result;
+        }
+    }
+}
