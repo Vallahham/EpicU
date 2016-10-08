b27204ffbb2185946136472844087616aee534ce
Violette Allahham <vallahham@yahoo.com>
2016-10-08 10:55:04 -0500

    foreach loop

--- CS-ASP_046/CS-ASP_046/Default.aspx.cs
+++ CS-ASP_046/CS-ASP_046/Default.aspx.cs
@@ -17,13 +17,15 @@ namespace CS_ASP_046
             Car car2 = new Car("BMW", "745li", 2005, "Black");
             Car car3 = new Car("ford", "Escape", 2008, "White");
 
-            var cars = new List<Car>()       //var instead of List<Car>     This code replaces code block above and code block below
+            var cars = new List<Car>();       //var instead of List<Car>     This code replaces code block above and code block below
+            /*
             {
                 new Car {Make = "", Model = "" },
                 new Car {Make = "", Model = "" },
                 new Car() {Make = "", Model = "" }      //Perenthesis on Car() not necessary here becasuse in code block and no perameters are being passed
 
             };
+            */
           
             cars.Add(car1);                  //In a list, not necessary to name item in a list unless neading to access it which can be done by going through the list with a unique identifier anyways.
             cars.Add(car2);
@@ -38,11 +40,18 @@ namespace CS_ASP_046
                 result += "We do have a 2008 model in stock!";
             }
 
+            /*
             for (int i = 0; i < cars.Count; i++)
             {
                 result += cars.ElementAt(i).FormatDetailsForDisplay();      //Better way to do this
             }
 
+            */
+
+            foreach (Car car in cars)                       //Use foreach for collections not for loop
+            {
+                result += car.FormatDetailsForDisplay();        //Going by reference instead of index[i]
+            }
             resultLabel.Text = result;
         }
     }
