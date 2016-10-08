ac9ea1d6243377672687d6dcbce1316bd7fd05b7
Violette Allahham <vallahham@yahoo.com>
2016-10-08 14:05:19 -0500

    up

---
+++ MegaChallengeWar/MegaChallengeWar/Deck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar
+{
+    public class Deck
+    {
+        //public int MyProperty { get; set; }
+
+        public void CardID()
+        {
+            for (int i = 1; i < 14; i++)
+            {
+
+            }
+        }
+        
+        public void Card()
+        {
+
+        }
+
+        public enum Suit
+        {
+            Spades,
+            Diamonds,
+            Clubs,
+            Hearts
+        }
+
+
+    }
+}
+
+//Use for loop to set unique identifier into each card
+
