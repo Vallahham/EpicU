9a8bf0e0faf00d374cf2f7fc3915c6677f6bcf32
Violette Allahham <vallahham@yahoo.com>
2016-10-15 12:54:48 -0500

    Day 6

---
+++ LocalDBExample/LocalDBExample/dbo.Customers.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Customers] 
+(
+    [Customer ID]  UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(),
+    [Name]         NVARCHAR (50)    NOT NULL,
+    [Address]      NVARCHAR (50)    NOT NULL,
+    [City]         NVARCHAR (50)    NOT NULL,
+    [State]        NVARCHAR (50)    NOT NULL,
+    [Postal Codes] NVARCHAR (50)    NOT NULL,
+    [Notes]        NVARCHAR (MAX)   NULL,
+
+);
+
